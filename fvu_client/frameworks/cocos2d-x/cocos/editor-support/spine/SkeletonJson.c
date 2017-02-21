e3080eceaa544af5e33715a8e5e46651ab5f1182
HeYuHan <435481457@qq.com>
2017-02-21 15:32:02 +0800

    spine bug

--- fvu_client/frameworks/cocos2d-x/cocos/editor-support/spine/SkeletonJson.c
+++ fvu_client/frameworks/cocos2d-x/cocos/editor-support/spine/SkeletonJson.c
@@ -853,6 +853,8 @@ spSkeletonData* spSkeletonJson_readSkeletonData (spSkeletonJson* self, const cha
 						type = SP_ATTACHMENT_REGION;
 					else if (strcmp(typeString, "mesh") == 0)
 						type = SP_ATTACHMENT_MESH;
+					else if (strcmp(typeString, "skinnedmesh") == 0)
+						type = SP_ATTACHMENT_SKINNED_MESH;
 					else if (strcmp(typeString, "linkedmesh") == 0)
 						type = SP_ATTACHMENT_LINKED_MESH;
 					else if (strcmp(typeString, "boundingbox") == 0)
@@ -900,6 +902,7 @@ spSkeletonData* spSkeletonJson_readSkeletonData (spSkeletonJson* self, const cha
 						spAttachmentLoader_configureAttachment(self->attachmentLoader, attachment);
 						break;
 					}
+					case SP_ATTACHMENT_SKINNED_MESH:
 					case SP_ATTACHMENT_MESH:
 					case SP_ATTACHMENT_LINKED_MESH: {
 						spMeshAttachment* mesh = SUB_CAST(spMeshAttachment, attachment);
